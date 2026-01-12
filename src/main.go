2ac5ab7db0ebdef19b6f8226da19379e0ab4c3b1
Raphael <raphaels10@hotmail.com>
2026-01-12 19:42:57 -0300

    updates response body for default endpoint

--- src/main.go
+++ src/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello from my K8s Web Server!")
+	fmt.Fprintf(w, "Hello from my K8s Web Server, v2!")
 }
 
 func main() {
